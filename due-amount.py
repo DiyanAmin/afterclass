b21977c14cfe0063337a48960b182c08103707a5
DiyanAmin <pakuptl3112@gmail.com>
2025-05-29 21:07:19 +0530

    fixed-again

--- due-amount.py
+++ due-amount.py
@@ -2,20 +2,12 @@ bill=float(input('Enter bill amount: '))
 paid=float(input('Enter amount you paid: '))
 def due_amount(paid,bill):
     print('Calculating..')
-    if paid<=bill:
+    if paid<bill:
         due=bill-paid
-        print('You need to pay',due,'more.')
-    elif paid>bill:
-        due=paid-bill
         print(due,'Change')
-    else:
-        print('ValueError: invalid literal for int() with base 10')
-#     if paid<=bill:
-#      due=bill-paid
-#      print('You need to pay',due,'more.')
-#  elif paid>bill:
-#      due=paid-bill
-#      print(due,'change')
-#  else:
-#      print('ValueError: invalid literal for int() with base 10')
+    elif paid>bill:
+        due1=paid-bill
+        print('Shopkeeper needs to return',due1,'more')
+    elif paid==bill:
+        print('Bill paid Successfully')
 due_amount(bill,paid)
