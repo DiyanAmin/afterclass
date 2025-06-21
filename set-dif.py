9898305eecdd16aa4483db68ef20422721d168e3
DiyanAmin <pakuptl3112@gmail.com>
2025-06-21 11:39:13 +0530

    completed set and fixed tuple

---
+++ set-dif.py
@@ -0,0 +1,9 @@
+#Symmetric dif between set 'A' and set 'B' and set 1 & 2 
+A={'blue','green'}
+B={'blue','yellow'}
+dif1=A.symmetric_difference(B)
+print(f'Symmetric difference between set A and B: {dif1}')
+one={1,2,3,4,5}
+two={1,5,6,7,8,9}
+dif2=one.symmetric_difference(two)
+print(f'Symmetric difference between set 1 and 2: {dif2}')
