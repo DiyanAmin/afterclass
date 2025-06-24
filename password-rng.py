7054f5cefb79345d25f391bfeaab6237ad3a9055
DiyanAmin <pakuptl3112@gmail.com>
2025-06-24 16:34:14 +0530

    completed

--- password-rng.py
+++ password-rng.py
@@ -4,15 +4,16 @@ l=[]
 password=[]
 characters=['1','2','3','4','5','6','7','8','9','!','@','#','$','%','^','&','*','*','()']
 empty=[]
-for i in string.ascii_letters:
+for i in string.ascii_letters:#---> Adding alphabets to empty[]
     empty.append(i)
-for k in range(len(characters)):
-    l.append(characters[k] + empty[k])
+for k in range(len(characters)):#---> Digit of password to change, remove len and replace with your number, tho given range gives stronger password
+    l.append(characters[k] + empty[k])#---> Merging alphabets and characters
 for j in range(len(l)):
     e=random.choice(l)
     password.append(e)
 # print(password)
-final_pass=''
+final_pass1=[]
 for jki in password:
-    final_pass.join(jki)
-print(final_pass)
+    final_pass1.append(jki)#---> Adding 'jki' to final_pass1
+final_pass=''.join(final_pass1)#---> Final Product creatted by combining all string in final_pass1
+print(f'Password: {final_pass}')
